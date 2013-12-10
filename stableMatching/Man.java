422fce1831e26360769ab86225ce32fd2e1126ad
Alan Ponte <alanjponte@gmail.com>
2013-12-10 12:45:08 -0800

    Update Man.java

--- stableMatching/Man.java
+++ stableMatching/Man.java
@@ -1,5 +1,5 @@
 package stableMatching;
-
+import java.util.Map;
 /** * A Man that will be matched with a woman in the Stable Marriage Algorithm.
 	* Every man has a prefrence list, indicated by an ArrayList.  The rank
 	* of the preferences is indicated by the position in the list.
@@ -9,32 +9,18 @@ package stableMatching;
 	*/
 	
 	
-public class Man {
+public class Man extends Agent {
 	
-	public static String name;
+	/** A new man whose Name is NAME with preferences PREFERENCES.*/
+	public Man(String Name, Map<Agent, Integer> preferences) {
+		super(Name, preferences);
+		
+	}
 	
-	/* constructor.  sets THIS man's NAME */
+	
+	/** A new man whose Name is NAME.*/
 	public Man(String Name) {
-		this.name = Name;	
-	}
-	
-	
-	private ArrayList <String> preferenceList = new ArrayList <String>();
-	boolean mathced = false;
-	
-	/** Returns TRUE iff given man is matched to any woman */
-	public bool isMatched(){
-		return matched;	
-	}
-	
-	/** Adds the WOMAN as the nth preference */
-	public static void addPrefrence(woman){
-		this.preferenceList.add(woman);
-	}
-	
-	/** Returns the Nth prefernce */
-	public static String getPreference (n){
-		return this.preferenceList.get(n);
+		super(Name);
 	}
 	
 	
