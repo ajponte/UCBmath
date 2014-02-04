3f4f585d35b053489c862572163b3a1b74a76b62
Alan Ponte <alanjponte@gmail.com>
2014-02-04 11:41:31 -0800

    Update NewtonSolver.java

--- newton/NewtonSolver.java
+++ newton/NewtonSolver.java
@@ -8,7 +8,6 @@ public class NewtonSolver {
 	/** Creates a new solver with the Polynoomial P, the 
 	 * derivative of the Polynomial, DF, and an initial guess of GUESS.
 	 * The solver will run until the TOLERANCE is reached.*/
-
 	public NewtonSolver(Polynomial p, Polynomial df, double guess, double tolerance) {
 		_poly = p;
 		_guess = guess;
@@ -16,20 +15,34 @@ public class NewtonSolver {
 		_tol = tolerance;
 	}
 	
+	/** Creates a new solver with the Polynoomial P, the 
+	 * derivative of the Polynomial, DF, and an initial guess of GUESS.
+	 * The solver will run until the TOLERANCE is reached, or the maximum number
+	 * of iterations. MAXITER is reached.*/
+	public NewtonSolver(Polynomial p, Polynomial df, double guess, double tolerance, int maxIter) {
+		_poly = p;
+		_guess = guess;
+		_df = df;
+		_tol = tolerance;
+		_maxIter = maxIter;
+	}
+
 	/** Returns the tolerance of THIS solver.*/
 	public double getTolerance() {
 		return _tol;
 	}
+
 	/**Returns the result of applying newton's method.*/
 	public double solve() {
 		int i = 0;
 		double p = 0.0;
 		while (true) {
-			p = _poly.valueAt(_guess)/_df.valueAt(_guess);
+			p = _guess - _poly.valueAt(_guess)/_df.valueAt(_guess);
+			System.out.println("P-Value: " + p + " itration : " + i);
 			if (Math.abs(p - _guess) < _tol) {
 				break;
 			}
-			p = _guess;
+			_guess = p;
 			i += 1;
 		}
 		return p;
@@ -39,5 +52,6 @@ public class NewtonSolver {
 	private Polynomial _df;
 	private double _guess;
 	private double _tol;
+	private int _maxIter;
 
 }
